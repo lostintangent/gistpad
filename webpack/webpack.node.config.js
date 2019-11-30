6b1227efb008d5dcdecae3903105fabb3acc1f32
Oleg Solomka <legomushroom@gmail.com>
2019-11-29 16:19:15 -0800

    address PR feedback

--- webpack/webpack.node.config.js
+++ webpack/webpack.node.config.js
@@ -1,10 +1,5 @@
-const webpack = require('webpack');
-
-const TerserPlugin = require('terser-webpack-plugin');
-
-const merge = require('webpack-merge');
-const devConfig = require('./webpack.config.js');
-
+const merge = require("webpack-merge");
+const devConfig = require("./webpack.config.js");
 
 module.exports = merge(devConfig, {
   // production config here

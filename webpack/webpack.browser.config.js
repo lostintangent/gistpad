0b13d23a0f1212f0dbfa1ae0690d828bd7155564
Oleg Solomka <legomushroom@gmail.com>
2019-11-29 16:21:06 -0800

    amend

--- webpack/webpack.browser.config.js
+++ webpack/webpack.browser.config.js
@@ -1,19 +1,16 @@
 const path = require('path');
-const webpack = require('webpack');
-
-const TerserPlugin = require('terser-webpack-plugin');
 
 const merge = require('webpack-merge');
 const devConfig = require('./webpack.config.js');
 
 module.exports = merge(devConfig, {
-    // target: 'webworker',
-    resolve: {
-        alias: {
-          '@abstractions': path.join(__dirname, '../src/abstractions/browser')
-        }
-    },
-    node: {
-      util: true
+  // target: 'webworker',
+  resolve: {
+    alias: {
+      '@abstractions': path.join(__dirname, '../src/abstractions/browser')
     }
+  },
+  node: {
+    util: true
+  }
 });

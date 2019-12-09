5bd0ed9f80c0df23a89196616aba9a506e3d48f3
Oleg Solomka <legomushroom@gmail.com>
2019-12-08 18:00:01 -0800

    address PR feedback

--- src/protocolHandler.ts
+++ src/protocolHandler.ts
@@ -1,46 +1,40 @@
-import { URLSearchParams } from 'url';
-import * as vscode from 'vscode';
-import { CommandId } from './constants';
-import { log } from './logger';
+import { URLSearchParams } from "url";
+import * as vscode from "vscode";
+import { CommandId } from "./constants";
+import { log } from "./logger";
 
 const getQuery = (uri: vscode.Uri): URLSearchParams => {
   const query = new URLSearchParams(uri.query);
 
   return query;
-}
+};
 
-const openGistFactory = (paramName: 'id' | 'url') => {
-  return async (uri: vscode.Uri) => {
-    const query = getQuery(uri);
+const openGist = async (uri: vscode.Uri) => {
+  const query = getQuery(uri);
 
-    const gistParam = query.get(paramName);
+  const gistUrl = query.get("url");
+  const gistId = query.get("id");
+  const openAsWorkspace = query.get("workspace") === "true";
 
-    if (!gistParam) {
-      log.error(`No gist ${paramName} specified in "${uri}"`);
-      throw new Error(`No gist ${paramName} specified.`);
-    }
-
-    const propName = (paramName === 'id')
-      ? 'gistId'
-      : 'gistUrl'
-
-    await vscode.commands.executeCommand(CommandId.openGist, { [propName]: gistParam });
+  if (!gistUrl && !gistId) {
+    const message = `No gist URL nor Id specified in "${uri}"`;
+    log.error(message);
+    throw new Error(message);
   }
-}
+
+  const options = gistUrl ? { gistUrl } : { gistId };
+
+  await vscode.commands.executeCommand(CommandId.openGist, {
+    ...options,
+    openAsWorkspace,
+  });
+};
 
 export class GistPadProtocolHandler implements vscode.UriHandler {
   public async handleUri(uri: vscode.Uri): Promise<void> {
     switch (uri.path) {
-      case '/open-gist': {
-        const openGist = openGistFactory('id');
-
+      case "/open-gist": {
         return await openGist(uri);
-      }
-
-      case '/open-gist-url': {
-        const openGistUrl = openGistFactory('url')
-
-        return await openGistUrl(uri);
       }
 
       default: {
@@ -49,12 +43,12 @@ export class GistPadProtocolHandler implements vscode.UriHandler {
     }
   }
 
-  public dispose(): void { }
+  public dispose(): void {}
 }
 
 export const initializeProtocolHander = () => {
-  if (typeof vscode.window.registerUriHandler === 'function') {
-    log.info('Protocol handler is registered.');
-    vscode.window.registerUriHandler(new GistPadProtocolHandler);
+  if (typeof vscode.window.registerUriHandler === "function") {
+    log.info("Protocol handler is registered.");
+    vscode.window.registerUriHandler(new GistPadProtocolHandler());
   }
-}
+};

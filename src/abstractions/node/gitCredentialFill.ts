ce7a8b5fefc20886746ba680f6c76a467d6f8029
Lukas Spieß <lukas.spiess@gmail.com>
2020-03-30 22:45:46 +0200

    Fix git credential fill command
    Removed leading whitespaces which lead to the command not being properly executed.
    
    When using
    
    ```sh
    protocol=https
      host=github.com
    
    ```
    
    as input for `git credential fill`, it simply queries the first entry in e.g. the macOS keychain that matches `protocol=https`, the `host=github.com` part is simply ignored.

--- src/abstractions/node/gitCredentialFill.ts
+++ src/abstractions/node/gitCredentialFill.ts
@@ -3,9 +3,9 @@ import { execSync } from "child_process";
 export function execGitCredentialFill() {
   // See here for more details: https://git-scm.com/docs/git-credential
   const input = `protocol=https
-  host=github.com
-  
-  `;
+host=github.com
+
+`;
 
   try {
     const response = execSync("git credential fill", {

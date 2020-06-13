235f9f6ef51e036cae6d56fe6fa2ef32e2446036
Mark Berry <markberry867@gmail.com>
2020-06-13 02:35:27 -0500

    Switch back to execSync with askpass-empty

--- src/abstractions/node/gitCredentialFill.ts
+++ src/abstractions/node/gitCredentialFill.ts
@@ -1,4 +1,27 @@
-import { execFileSync } from "child_process";
+import { execSync } from "child_process";
+import * as path from "path";
+import * as vscode from "vscode";
+
+function getAskPassEnv(useAskPassEmpty = true) {
+  const gitExt = vscode.extensions.getExtension("vscode.git");
+  const gitExtPath = gitExt?.extensionPath;
+
+  if (!gitExtPath) {
+    return {};
+  }
+
+  if (useAskPassEmpty) {
+    return {
+      GIT_ASKPASS: path.join(gitExtPath, "dist", "askpass-empty.sh")
+    };
+  }
+
+  return {
+    GIT_ASKPASS: path.join(gitExtPath, "dist", "askpass.sh"),
+    VSCODE_GIT_ASKPASS_NODE: process.execPath,
+    VSCODE_GIT_ASKPASS_MAIN: path.join(gitExtPath, "dist", "askpass.sh")
+  };
+}
 
 export function execGitCredentialFill() {
   // See here for more details: https://git-scm.com/docs/git-credential
@@ -8,10 +31,14 @@ host=github.com
 `;
 
   try {
-    const response = execFileSync("git", ["credential", "fill"], {
+    const response = execSync("git credential fill", {
       input,
-      encoding: "utf8"
+      encoding: "utf8",
+      env: {
+        ...getAskPassEnv()
+      }
     });
+
     return response.split("password=")[1].trim();
   } catch (e) {
     return;

abb48ff756c10e20a667b505be149ec651f7eac8
Jonathan Carter <joncart@microsoft.com>
2020-07-11 15:42:41 -0700

    Correcting typo

--- src/repos/index.ts
+++ src/repos/index.ts
@@ -6,6 +6,7 @@ import { refreshRepositories } from "./store/actions";
 import { initializeStorage } from "./store/storage";
 import { registerTourController } from "./tours";
 import { registerTreeProvider } from "./tree";
+import { registerWikiController } from "./wiki";
 
 export function registerRepoModule(context: vscode.ExtensionContext) {
   registerRepoCommands(context);
@@ -18,7 +19,7 @@ export function registerRepoModule(context: vscode.ExtensionContext) {
   registerCommentController(context);
   registerTourController(context);
 
-  //registerWikiController(context);
+  registerWikiController(context);
 
   refreshRepositories();
 }

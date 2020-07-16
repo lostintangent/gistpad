39c55e55d1326680d662ff89dc2abdf802dbcc9c
Jonathan Carter <joncart@microsoft.com>
2020-07-16 15:58:28 -0700

    Misc. feedback items

---
+++ src/repos/wiki/config.ts
@@ -0,0 +1,15 @@
+import { workspace } from "vscode";
+import { EXTENSION_NAME } from "../../constants";
+
+function getConfigSetting(settingName: string) {
+  return workspace.getConfiguration(EXTENSION_NAME).get(`wikis.${settingName}`);
+}
+
+export const config = {
+  get dailyDirectName() {
+    return getConfigSetting("daily.directoryName");
+  },
+  get dailyTitleFormat() {
+    return getConfigSetting("daily.titleFormat");
+  }
+};

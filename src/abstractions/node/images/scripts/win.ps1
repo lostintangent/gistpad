cb985c6ba83802c03da3a9c85d02251f2bba14b2
Berkay GURSOY <berkaygursoy@gmail.com>
2021-03-16 01:05:59 +0300

    Fixed windows clipboard bug

--- src/abstractions/node/images/scripts/win.ps1
+++ src/abstractions/node/images/scripts/win.ps1
@@ -10,11 +10,6 @@ if ($img -eq $null) {
     Exit 1
 }
 
-if (-not $imagePath) {
-    "no image"
-    Exit 1
-}
-
 $fcb = new-object Windows.Media.Imaging.FormatConvertedBitmap($img, [Windows.Media.PixelFormats]::Rgb24, $null, 0)
 $stream = [IO.File]::Open($imagePath, "OpenOrCreate")
 $encoder = New-Object Windows.Media.Imaging.PngBitmapEncoder

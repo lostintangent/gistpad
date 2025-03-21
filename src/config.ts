ade9be9440161afab865f087ca38a9739186c027
Jonathan Carter <jc@trl.mx>
2025-03-21 07:26:23 -0700

    Adding MCP integration

--- src/config.ts
+++ src/config.ts
@@ -2,17 +2,20 @@ import * as vscode from "vscode";
 
 const CONFIG_SECTION = "gistpad";
 
-export function get(key: "treeIcons"): boolean;
+export function get(key: "comments.showThread"): string;
 export function get(key: "images.markdownPasteFormat"): "markdown" | "html";
 export function get(key: "images.pasteType"): "file" | "base64";
 export function get(key: "images.directoryName"): string;
+export function get(key: "mcp.enabled"): boolean;
+export function get(key: "mcp.markdownOnly"): boolean;
+export function get(key: "output"): boolean;
 export function get(key: "scratchNotes.directoryFormat"): string;
 export function get(key: "scratchNotes.fileExtension"): string;
 export function get(key: "scratchNotes.fileFormat"): string;
 export function get(key: "scratchNotes.show"): boolean;
 export function get(key: "showcaseUrl"): string;
-export function get(key: "comments.showThread"): string;
-export function get(key: "output"): boolean;
+export function get(key: "treeIcons"): boolean;
+
 export function get(key: any) {
   const extensionConfig = vscode.workspace.getConfiguration(CONFIG_SECTION);
   return extensionConfig.get(key);

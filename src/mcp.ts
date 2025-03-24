56afa7d2e562fe4acac7d7f4e1ada451e0243e83
Jonathan Carter <jc@trl.mx>
2025-03-24 08:25:04 -0700

    Adding MCP config provider

---
+++ src/mcp.ts
@@ -0,0 +1,58 @@
+import { reaction } from "mobx";
+import {
+    EventEmitter,
+    ExtensionContext,
+    lm,
+    McpStdioServerDefinition,
+    workspace
+} from "vscode";
+import * as config from "./config";
+import { store } from "./store";
+
+export function registerMcpConfigurationProvider(context: ExtensionContext) {
+    const onDidChange = new EventEmitter<void>();
+
+    // When the user signs in or update, let VS Code
+    // know that it needs to refresh the MCP configuration.
+    reaction(
+        () => store.isSignedIn,
+        () => {
+            onDidChange.fire();
+        }
+    );
+
+    // When the user updates any of the GistPad MCP config
+    // settings, let VS Code know that it needs to refresh.
+    context.subscriptions.push(
+        workspace.onDidChangeConfiguration((e) => {
+            if (e.affectsConfiguration("gistpad.mcp")) {
+                onDidChange.fire();
+            }
+        })
+    );
+
+    context.subscriptions.push(
+        lm.registerMcpConfigurationProvider("gistpad", {
+            onDidChange: onDidChange.event,
+            provideMcpServerDefinitions() {
+                if (!store.isSignedIn || !config.get("mcp.enabled")) return [];
+
+                const args = ["-y", "gistpad-mcp"];
+                if (config.get("mcp.markdownOnly")) {
+                    args.push("--markdown");
+                }
+
+                const mcpServerConfig: McpStdioServerDefinition = {
+                    label: "GistPad",
+                    command: "npx",
+                    args,
+                    env: {
+                        GITHUB_TOKEN: store.token!
+                    }
+                };
+
+                return [mcpServerConfig];
+            }
+        })
+    );
+}
